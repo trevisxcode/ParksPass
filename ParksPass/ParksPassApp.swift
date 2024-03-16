16df65b757d1d02e8713c261683e1515cf60f857
Trevis Suwanwigo <s.taeshawit@gmail.com>
2024-03-16 11:13:39 -0700

    Initial Commit

---
+++ ParksPass/ParksPassApp.swift
@@ -0,0 +1,12 @@
+// @copyright ParksPass by TrevisXcode
+
+import SwiftUI
+
+@main
+struct ParksPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
